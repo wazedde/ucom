2d918193c3e9c20e69f6df13a481be0eed281865
Jakko <26172288+wazedde@users.noreply.github.com>
2023-11-24 09:33:12 +0100

    Added header to UnityBuilder script

--- src/commands/include/UnityBuilder.cs
+++ src/commands/include/UnityBuilder.cs
@@ -1,3 +1,22 @@
+/*
+ * This file contains the UnityBuilder class which handles build capabilities for Unity projects and is part of the
+ * ucom command line tool (https://github.com/jakkovanhunen/ucom).
+ *
+ * Copyright 2022-2023 Jakko van Hunen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -37,7 +56,7 @@ namespace Ucom
         private const string BuildOptionsArg = "--ucom-build-options";
 
         /// <summary>
-        /// Custom arguments passed to the build script.
+        /// Custom arguments passed to the build scripts.
         /// </summary>
         private const string PreBuildArgs = "--ucom-pre-build-args";
 
@@ -350,7 +369,7 @@ namespace Ucom
         {
             return AppDomain.CurrentDomain
                             .GetAssemblies()
-                            .SelectMany(assembly => assembly.GetTypes(), (_, type) => type)
+                            .SelectMany(assembly => assembly.GetTypes())
                             .SelectMany(GetPreProcessBuildMethods)
                             .All(method => InvokeMethod(method, arg));
         }
